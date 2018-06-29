760ec2b50b5e2c7d53606c1039a33fa5bc6cbb78
Andreas <alantsov@gmail.com>
2018-06-29 19:02:15 +0300

    Revert "Revert "добавил таблице со странами""
    This reverts commit 90abfa946b475b9c95d822fd1d367edc77be33db.

--- resources/views/_form.blade.php
+++ resources/views/_form.blade.php
@@ -15,6 +15,11 @@
                 </div>
 
                 <div class="form-group">
+                    <label >Country</label>
+                        <input type="text" name="country" class="form-control" rows="1" value="">
+                </div>
+
+                <div class="form-group">
                     <label for="exampleFormControlTextarea1">Details</label>
                     <textarea name="details" class="form-control" rows="6" >{{$events->details}}</textarea>
                 </div>
