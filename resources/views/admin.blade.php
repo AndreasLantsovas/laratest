fc57382cf27848a818e7302ac8f3e7c8ab175682
Andreas <alantsov@gmail.com>
2018-06-29 19:18:40 +0300

    Revert "добавил беджи к именам"
    This reverts commit fee155b9a5486663d7926a78e3320d922bece99c.

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -27,21 +27,13 @@
                 <div class="card">
                     <div class="card-body">
                         <h3 class="card-title">{{$event->name}}</h2>
-                        
-                        @if ($event->published === 1)
-                          <span class="badge badge-pill badge-warning">published</span>
-                          @else
-                          
-                        @endif
-
                         <h6>Start at {{$event->start_date}}</h6>
                         <p>{{$event->details}}</p>
                         <p>
-                            <a href="{{URL::action('EventController@show', $event->alias)}}" class="btn btn-primary btn-sm">Preview</a>
+                            <a href="{{URL::action('EventController@show', $event->alias)}}" class="btn btn-primary btn-sm">More</a>
                             <a href="{{URL::action('AdminController@edit', $event->id)}}" class="btn btn-success btn-sm">Edit</a>
                             <a href="{{URL::action('AdminController@destroy', $event->id)}}" class="btn btn-danger btn-sm">Delete</a>
                         </p>
-                        <hr>
                     </div>
                 </div>
             </div>
